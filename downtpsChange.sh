6eac35cdd77192daba06430ce1f77a15f7c548a6
jrmadscience <jadejakajal13@gmail.com>
2023-10-06 11:00:09 +0100

    first commit

---
+++ downtpsChange.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Name of the ConfigMap
+configmap_name="kvbenchmark-config"
+
+# Key to update
+key_to_update="TPS"
+
+# New value
+new_value="500"
+
+# Create a temporary ConfigMap YAML file
+cat <<EOF > temp-configmap.yaml
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: $configmap_name
+data:
+  $key_to_update: "$new_value"
+EOF
+
+# Apply the updated ConfigMap using kubectl apply
+kubectl apply -f temp-configmap.yaml
+
+# Clean up the temporary file
+rm temp-configmap.yaml
+
+echo "ConfigMap $configmap_name updated with $key_to_update=$new_value"
+
+sleep 2
+
+kubectl rollout restart deployment voltkv 
